86bc38c7b4d989890604b5148bd1cacf48848a2f
Ben Abbott <info@ben-abbott.co.uk>
2021-02-02 21:10:16 +0000

    Start adding unit tests

---
+++ Umbraco.Homework.API.Test/ConfigControllerTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using Umbraco.Homework.API.Controllers;
+using Umbraco.Homework.API.Models;
+using Xunit;
+
+// [Theory] you pass in data
+// [Fact] no data passed in to the method
+
+// TEST DOUBLES
+// Fakes - working implementations of functionality not fit for prd
+// Dummies - can be created and passed arround but never used by the method we are testing
+// Stubs - create stubs and pass them as dependencies to provide answers to calls on functionality
+// Mocks - ??
+
+// MockBehaviour.Strict - will throw an exception if a method is called on the mock object that hasn't been setup
+// MockBehaviour.Loose (Default) - won't throw
+// Example: Mock<IRepository<Bitmap>> mockImgRepo = new Mock<IRepository<Bitmap>>(MockBehavior.Strict);
+
+namespace Umbraco.Homework.API.Test
+{
+    public class ConfigControllerTests
+    {
+        [Fact]
+        public void TestInstantiation()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>())
+                .Build();
+
+            ConfigController controller = new ConfigController(configuration);
+
+            Assert.NotNull(controller);
+        }
+
+        [Fact]
+        public void TestGetConfigResult()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>())
+                .Build();
+
+            ConfigController controller = new ConfigController(configuration);
+
+            IActionResult test = controller.Get();
+
+            var okResult = test as OkObjectResult;
+
+            // assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+        }
+
+        [Fact]
+        public void TestMaxAllowedPrizeDrawEntries()
+        {
+            var inMemorySettings = new Dictionary<string, string>
+            {
+                { "MaxAllowedPrizeDrawEntries", "2" }
+            };
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
+
+            ConfigController controller = new ConfigController(configuration);
+
+            IActionResult test = controller.Get();
+
+            OkObjectResult okResult = test as OkObjectResult;
+
+            Config config = okResult.Value as Config;
+
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.Equal(2, config.MaxSubmissions);
+        }
+
+        // Can't get this working :(
+        /*
+        [Fact]
+        public void TestValidation()
+        {
+            var inMemorySettings = new Dictionary<string, string>
+            {
+                { "MaxAllowedPrizeDrawEntries", "2" },
+                { "Validation", "\"FirstNameRules\": [ { \"Regex\": \"\\S\", \"ErrorMessage\": \"First name is mandatory\" } ]" }
+                                
+            };
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
+
+            ConfigController controller = new ConfigController(configuration);
+
+            IActionResult test = controller.Get();
+
+            OkObjectResult okResult = test as OkObjectResult;
+
+            Config config = okResult.Value as Config;
+
+            // assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.NotNull(config.Validation.FirstNameRules);
+        }
+        */
+    }
+}
---
+++ Umbraco.Homework.API.Test/SerialNumberTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Umbraco.Homework.API.Controllers;
+using Umbraco.Homework.API.Data;
+using Umbraco.Homework.API.Models;
+using Umbraco.Homework.API.Services;
+using Xunit;
+using System.Linq;
+
+namespace Umbraco.Homework.API.Test
+{
+
+    // [Theory] you pass in data
+    // [Fact] no data passed in to the method
+
+    // TEST DOUBLES
+    // Fakes - working implementations of functionality not fit for prd
+    // Dummies - can be created and passed arround but never used by the method we are testing
+    // Stubs - create stubs and pass them as dependencies to provide answers to calls on functionality
+    // Mocks - ??
+
+    // MockBehaviour.Strict - will throw an exception if a method is called on the mock object that hasn't been setup
+    // MockBehaviour.Loose (Default) - won't throw
+    // Example: Mock<IRepository<Bitmap>> mockImgRepo = new Mock<IRepository<Bitmap>>(MockBehavior.Strict);
+
+    public class SerialNumberTests
+    {
+        [Fact]
+        public void TestInstantiation()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>())
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<PrizeDrawDbContext>();
+
+            optionsBuilder.UseInMemoryDatabase("PrizeDrawDatabseName");
+
+            var context = new PrizeDrawDbContext(optionsBuilder.Options);
+
+            ISerialNumberService serialNumberService = new SerialNumberService(context, configuration);
+
+            SerialNumberController controller = new SerialNumberController(serialNumberService);
+
+            Assert.NotNull(controller);
+        }
+
+        /*
+            IActionResult test = controller.Get();
+
+            OkObjectResult okResult = test as OkObjectResult;
+
+            Config config = okResult.Value as Config;
+
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.Equal(2, config.MaxSubmissions);
+        */
+
+        [Fact]
+        public void TestGenerateSerialNumberRange()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>())
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<PrizeDrawDbContext>();
+
+            optionsBuilder.UseInMemoryDatabase("PrizeDrawDatabseName");
+
+            var context = new PrizeDrawDbContext(optionsBuilder.Options);
+
+            ISerialNumberService serialNumberService = new SerialNumberService(context, configuration);
+
+            SerialNumberController controller = new SerialNumberController(serialNumberService);
+
+            IActionResult test = controller.GenerateSerialNumberRange().Result;
+
+            OkObjectResult okResult = test as OkObjectResult;
+
+            IEnumerable<SerialNumber> serialNumbers = okResult.Value as IEnumerable<SerialNumber>;
+
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.Equal(100, serialNumbers.Count());
+
+            Assert.NotNull(controller);
+        }
+    }
+}
