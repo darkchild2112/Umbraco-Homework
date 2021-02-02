8ac609372f5841bb7331d07fae093febbd35bc61
Ben Abbott <info@ben-abbott.co.uk>
2021-02-02 15:57:45 +0000

    Add datetime to entry + generate serial number bug

---
+++ Umbraco.Homework.API/Migrations/20210202155134_addSubmittedDate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Umbraco.Homework.API.Migrations
+{
+    public partial class addSubmittedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SerialNumber",
+                table: "PrizeDrawEntries",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Submitted",
+                table: "PrizeDrawEntries",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Submitted",
+                table: "PrizeDrawEntries");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SerialNumber",
+                table: "PrizeDrawEntries",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
